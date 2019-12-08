d47bf0f8463df9c63abcf7902d0917ed7866c69f
Nochnoe Nebo <alabamaster1@gmail.com>
2019-12-08 15:28:08 +0300

    Add files via upload

--- maps.php
+++ maps.php
@@ -1,6 +1,5 @@
 <?php 
-require 'inc/config.php';
-require 'inc/func.php';
+require_once 'inc/func.php';
 
 $Time = new TimePlayers();
 ?>
@@ -12,7 +11,7 @@ $Time = new TimePlayers();
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
 		<!-- Font Awesome Icons -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/64ff6e1601.css">
@@ -21,8 +20,15 @@ $Time = new TimePlayers();
 		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap&subset=latin-ext" rel="stylesheet">
 
 		<!-- Main CSS -->
-		<link rel="stylesheet" href="<?=$main['url']?>template/css/main-fc.css">
-		<link rel="stylesheet" href="<?=$main['url']?>template/css/flag-icon.css">
+		<link rel="stylesheet" href="template/css/main-fc.css">
+		<link rel="stylesheet" href="template/css/flag-icon.css">
+
+		<style>
+			td span {
+				position: relative;
+				top: 15px;
+			}
+		</style>
 
 		<title>Статистика карт</title>
 		<!-- <meta name="description" content="">-->
@@ -48,7 +54,7 @@ $Time = new TimePlayers();
 						<?php 
 							$maps = DB::run('SELECT `player_id`, `map`, SUM(kills) AS `kills`, SUM(deaths) AS `deaths`, SUM(dmg) AS dmg, SUM(connection_time) AS `connection_time` FROM `csstats_maps` GROUP BY `map` ORDER BY `kills` DESC');
 						?>
-						<div class="table-responsive-md">
+						<div class="table-responsive">
 							<table class="table">
 								<thead>
 									<tr>
@@ -81,46 +87,50 @@ $Time = new TimePlayers();
 									//echo $httpcode;
 
 									$mapimage = ( $httpcode == 200 ) ? $url : 'https://image.gametracker.com/images/maps/160x120/nomap.jpg';
-
-									echo '<tr>';
-									echo '<td><div class="media">';
-									echo '<img src="'. $mapimage .'" class="mapimage mr-3">';
-									echo '<div class="media-body">';
-									echo '<p style="padding-top: 10px;"><a href="#" data-toggle="modal" data-target="#map_id'.$id.'">'.$r['map'].'</a></p>';
-									echo '</div>';
-									echo '</div></td>';
-									echo '<td><span class="txt1">' . number_format($r['kills']) . '</span></td>';
-									echo '<td><span class="txt1">' . number_format($r['deaths']) . '</span></td>';
-									echo '<td><span class="txt1">' . number_format($r['dmg']) . '</span></td>';
-									echo '<td><span class="txt1">' . $Time->TimeOn($r['connection_time']) . '</span></td>';
-							?>
-
-								<div class="modal" id="map_id<?=$id?>" tabindex="-1" role="dialog" aria-hidden="true">
-										<div class="modal-dialog modal-xl">
-											<div class="modal-content">
-												<div class="modal-header">
-													<h5 class="modal-title" id="exampleModalLabel">Топ лучших игроков карты: <?=$r['map']?></h5>
-													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-													<span aria-hidden="true">&times;</span>
-													</button>
+									?>
+									<tr>
+										<td>
+											<div class="media">
+												<img src="<?=$mapimage;?>" class="mr-3 rounded" style="width: 70px;">
+												<div class="media-body">
+													<p style="padding-top: 10px;">
+														<a style="position: relative;top: 5px" href="#" data-toggle="modal" data-target="#map_id<?=$id;?>"><?=$r['map'];?></a>
+													</p>
 												</div>
-												<div class="modal-body">
-													<div class="row">
-														<div class="col">
-															<?php 
-																$row_id = 1;
-																$asd = $asd->FetchAll(PDO::FETCH_ASSOC);
-																foreach ($asd as $row_asd) {
-		echo '<p style="border-bottom: 1px dashed #ccc;">#'.$row_id.' <a href="'.$main['url'].'user.php?id='.$row_asd['id'].'">'.$row_asd['name'].'</a> заработал <b style="color:#00b707">'.$row_asd['kills'].'</b> фрагов, умер <b style="color:#ff5e5e">'.$row_asd['deaths'].'</b> раз, играл на карте <b style="color:#3899ff">'.$Time->TimeOn($row_asd['connection_time']).'</b></p><br>';
-																	$row_id++;
-																}
-															?>
-														</div>
+											</div>
+										</td>
+										<td><span><?=number_format($r['kills']);?></span></td>
+										<td><span><?=number_format($r['deaths']);?></span></td>
+										<td><span><?=number_format($r['dmg']);?></span></td>
+										<td><span><?=$Time->TimeOn($r['connection_time']);?></span></td>
+									</tr>
+								
+								<div class="modal" id="map_id<?=$id?>" tabindex="-1" role="dialog" aria-hidden="true">
+									<div class="modal-dialog modal-xl">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title" id="exampleModalLabel">Топ лучших игроков карты: <?=$r['map']?></h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">
+												<div class="row">
+													<div class="col">
+														<?php 
+															$row_id = 1;
+															$asd = $asd->FetchAll(PDO::FETCH_ASSOC);
+															foreach ($asd as $row_asd) {
+	echo '<p style="border-bottom: 1px dashed #ccc;">#'.$row_id.' <a href="'.$main['url'].'user.php?id='.$row_asd['id'].'">'.$row_asd['name'].'</a> заработал <b style="color:#00b707">'.$row_asd['kills'].'</b> фрагов, умер <b style="color:#ff5e5e">'.$row_asd['deaths'].'</b> раз, играл на карте <b style="color:#3899ff">'.$Time->TimeOn($row_asd['connection_time']).'</b></p><br>';
+																$row_id++;
+															}
+														?>
 													</div>
 												</div>
 											</div>
 										</div>
 									</div>
+								</div>
 								<?php  $id++; } ?>
 								</tbody>
 							</table>
@@ -131,8 +141,8 @@ $Time = new TimePlayers();
 		</div>
 		<!-- JavaScript -->
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 		<!-- JS -->
 		<script type="text/javascript">
 			$(function () {
--- menu.php
+++ menu.php
@@ -1,3 +1,2 @@
-<?php require 'inc/config.php'; ?>
 <a class="nav-item nav-link" href="<?=$main['url']?>"> Игроки</a>
 <a class="nav-item nav-link" href="<?=$main['url']?>maps.php"> Карты</a>
